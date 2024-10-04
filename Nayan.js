c8d33f026b67e4be3f9e0659a4eef4a330941e1d
Userrobi <e3mon3@gmail.com>
2024-10-05 01:10:46 +0600

    Update Nayan.js

--- Nayan.js
+++ Nayan.js
@@ -1,165 +1,101 @@
-module.exports = async ({ api, event }) => {
-  const logger = require('./Nayan/catalogs/Nayanc.js')
-  
-  const configCustom = {
-    autosetbio: {
-      status: false,
-      bio: `prefix : ${global.config.PREFIX}`,
-      note: 'automatically change the bot bio.'
+[
+    {
+        "key": "datr",
+        "value": "K0tjZsF_ybjGtF3lZrRbte6I",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-10-04T19:09:47.382Z",
+        "lastAccessed": "2024-10-04T19:09:47.388Z"
     },
-    notification: {
-      status: false,
-      time: 39, // 39 minutes
-      note: 'bot will update you on his informations like all users, all groups, all operators, all admins every 30 minutes'
+    {
+        "key": "sb",
+        "value": "K0tjZjF2TaiK7qAB2DCC61bq",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-10-04T19:09:47.388Z",
+        "lastAccessed": "2024-10-04T19:09:47.388Z"
     },
-    greetings: {
-      status: false,
-      morning: `goodmorning everyone, have a nice day.`,
-      afternoon: `goodafternoon everyone, don't forget to eat your lunch.`,
-      evening: `goodevening everyone, don't forget to eat.`,
-      sleep: `goodnight everyone, time to sleep.`,
-      note: 'greetings every morning, afternoon and evening. the timezone is located in Asia/Dhaka'
+    {
+        "key": "ps_n",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-10-04T19:09:47.388Z",
+        "lastAccessed": "2024-10-04T19:09:47.388Z"
     },
-    reminder: {
-      status: false,
-      time: 40, // 40 minutes
-      msg: 'reminder test',
-      note: 'this is a reminder for 40 minutes, you can disabled it by setting the status to false'
+    {
+        "key": "ps_l",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-10-04T19:09:47.388Z",
+        "lastAccessed": "2024-10-04T19:09:47.388Z"
     },
-    autoDeleteCache: {
-      status: true,
-      time: 10, // 10 minutes
-      note: 'auto delete caches, kindly set the status to true, if you dont want to delete caches, set the status to false.'
+    {
+        "key": "vpd",
+        "value": "v1%3B668x360x2",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-10-04T19:09:47.388Z",
+        "lastAccessed": "2024-10-04T19:09:47.388Z"
     },
-    autoRestart: {
-      status: true,
-      time: 40, // 40 minutes
-      note: 'to avoid problems, enable periodic bot restarts, set the status to false if you want to disable auto restart function.'
+    {
+        "key": "wd",
+        "value": "360x668",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-10-04T19:09:47.388Z",
+        "lastAccessed": "2024-10-04T19:09:47.388Z"
     },
-    accpetPending: {
-      status: false,
-      time: 10, // 10 minutes
-      note: 'approve waiting messages after a certain time, set the status to false if you want to disable auto accept message request.'
+    {
+        "key": "c_user",
+        "value": "61555422556993",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-10-04T19:09:47.388Z",
+        "lastAccessed": "2024-10-04T19:09:47.388Z"
+    },
+    {
+        "key": "fr",
+        "value": "0505LtGL3UHAY81Yp.AWU0r25NkziLqttcYPksrZf39gg.Bm-Z0h..AAA.0.0.Bm-Z1F.AWXOnS9dReQ",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-10-04T19:09:47.388Z",
+        "lastAccessed": "2024-10-04T19:09:47.388Z"
+    },
+    {
+        "key": "xs",
+        "value": "21%3A7-7_3BXoJIhCAw%3A2%3A1727634757%3A-1%3A5127",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-10-04T19:09:47.388Z",
+        "lastAccessed": "2024-10-04T19:09:47.388Z"
+    },
+    {
+        "key": "fbl_st",
+        "value": "100439454%3BT%3A28796216",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-10-04T19:09:47.388Z",
+        "lastAccessed": "2024-10-04T19:09:47.388Z"
+    },
+    {
+        "key": "wl_cbv",
+        "value": "v2%3Bclient_version%3A2635%3Btimestamp%3A1727772968",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-10-04T19:09:47.388Z",
+        "lastAccessed": "2024-10-04T19:09:47.388Z"
     }
-  }
-
-  function autosetbio(config) {
-    if (config.status) {
-      try {
-        api.changeBio(config.bio, (err) => {
-          if (err) {
-            logger(`having some unexpected error : ${err}`, 'setbio')
-          }; return logger(`changed the bot bio into : ${config.bio}`, 'setbio')
-        })
-      } catch (error) {
-        logger(`having some unexpected error : ${error}`, 'setbio')
-      }
-    }
-  }
-  function notification(config) {
-    if (config.status) {
-      setInterval(async () => {
-        const operator = global.config.OPERATOR[0];
-        api.sendMessage(`bot information\n\nusers : ${global.data.allUserID.length}\ngroups : ${global.data.allThreadID.length}\noperators : ${global.config.OPERATOR.length}\nadmins : ${global.config.ADMINBOT.length}`, operator)
-      }, config.time * 60 * 1000)
-    }
-  }
-  function greetings(config) {
-    if (config.status) {
-      try {
-      const nam = [
-        {
-          timer: '6:00:00 AM',
-          message: [`${config.morning}`]
-        },
-        {
-          timer: '12:00:00 AM',
-          message: [`${config.afternoon}`]
-        },
-        {
-          timer: '6:00:00 PM',
-          message: [`${config.evening}`]
-        },
-        {
-          timer: '10:00:00 PM',
-          message: [`${config.sleep}`]
-        }
-      ];
-        setInterval(() => {
-const r = a => a[Math.floor(Math.random()*a.length)];
-if (á = nam.find(i => i.timer == new Date(Date.now()+25200000).toLocaleString().split(/,/).pop().trim())) global.data.allThreadID.forEach(i => api.sendMessage(r(á.message), i));
-}, 1000);
-      } catch (error) {
-        logger(`having some unexpected error : ${error}`, 'greetings')
-      }
-    }
-  }
-  function reminder(config) {
-    if (config.status) {
-      setInterval(async () => {
-        let allThread = global.data.allThreadID || [];
-        await new Promise(resolve => {
-          allThread.forEach((each) => {
-            try {
-              api.sendMessage(config.msg, each, (err, info) => {
-                if (err) {
-                  logger(`having some unexpected error : ${err}`, 'reminder')
-                }
-              })
-            } catch (error) {
-              logger(`having some unexpected error : ${error}`, 'reminder')
-            }
-          })
-        })
-      }, config.time * 60 * 1000)
-    }
-  }
-  function autoDeleteCache(config) {
-    if(config.status) {
-      setInterval(async () => {
-        const { exec } = require('child_process');
-        exec('rm -rf ../../scripts/commands/cache && mkdir -p ../../scripts/commands/cache && rm -rf ../../scripts/events/cache && mkdir -p ../../scripts/events/cache', (error, stdout, stderr) => {
-        if (error) {
-          logger(`error : ${error}`, "cache")
-          return;
-        }
-        if (stderr) {
-          logger(`stderr : ${stderr}`, "cache")
-          return;
-        }
-        return logger(`successfully deleted caches`, "cache")
-        })
-      }, config.time * 60 * 1000)
-    }
-  }
-  function autoRestart(config) {
-    if(config.status) {
-      setInterval(async () => {
-        logger(`auto restart is processing, please wait.`, "Nayan")
-        process.exit(1)
-      }, config.time * 60 * 1000)
-    }
-  }
-  function accpetPending(config) {
-    if(config.status) {
-      setInterval(async () => {
-          const list = [
-              ...(await api.getThreadList(1, null, ['PENDING'])),
-              ...(await api.getThreadList(1, null, ['OTHER']))
-          ];
-          if (list[0]) {
-              api.sendMessage('this thread is automatically approved by our system.', list[0].threadID);
-          }
-      }, config.time * 60 * 1000)
-    }
-  }
-
-autosetbio(configCustom.autosetbio)
-notification(configCustom.notification)
-greetings(configCustom.greetings)
-reminder(configCustom.reminder)
-autoDeleteCache(configCustom.autoDeleteCache)
-autoRestart(configCustom.autoRestart)
-accpetPending(configCustom.accpetPending)
-	
-};
+]
